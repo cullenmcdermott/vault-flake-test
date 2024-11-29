b757d93aa0f55cbcd768ca2acdbafadb6598341e
cullenmcdermott <9535761+cullenmcdermott@users.noreply.github.com>
2024-11-29 16:40:58 -0700

    more systems

--- flake.nix
+++ flake.nix
@@ -10,40 +10,65 @@
       self,
       nixpkgs,
     }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      # Helper to generate outputs for each system
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
     {
-      packages.aarch64-darwin.vault =
-        let
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        in
-        pkgs.buildGoModule {
-          pname = "vault";
-          version = "1.9.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "hashicorp";
-            repo = "vault";
-            rev = "v1.17.0";
-            sha256 = null;
-          };
-          vendorHash = "sha256-VUiBfJ+AJqnuBt5SawjVcldeMnRonxh0/JBNVPbLk+U=";
+      packages = forAllSystems (system: {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          pkgs.buildGoModule {
+            pname = "vault";
+            version = "1.9.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "hashicorp";
+              repo = "vault";
+              rev = "v1.17.0";
+              sha256 = null;
+            };
+            vendorHash = "sha256-VUiBfJ+AJqnuBt5SawjVcldeMnRonxh0/JBNVPbLk+U=";
 
-          subPackages = [ "." ];
-        };
-      packages.aarch64-linux.vault =
-        let
-          pkgs = nixpkgs.legacyPackages.aarch64-linux;
-        in
-        pkgs.buildGoModule {
-          pname = "vault";
-          version = "1.9.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "hashicorp";
-            repo = "vault";
-            rev = "v1.17.0";
-            sha256 = null;
+            subPackages = [ "." ];
           };
-          vendorHash = "sha256-VUiBfJ+AJqnuBt5SawjVcldeMnRonxh0/JBNVPbLk+U=";
-
-          subPackages = [ "." ];
-        };
+      });
     };
+  #pkgs.buildGoModule {
+  #  pname = "vault";
+  #  version = "1.9.1";
+  #  src = pkgs.fetchFromGitHub {
+  #    owner = "hashicorp";
+  #    repo = "vault";
+  #    rev = "v1.17.0";
+  #    sha256 = null;
+  #  };
+  #  vendorHash = "sha256-VUiBfJ+AJqnuBt5SawjVcldeMnRonxh0/JBNVPbLk+U=";
+
+  #  subPackages = [ "." ];
+  #  #packages.aarch64-linux.vault =
+  #  #  let
+  #  #    pkgs = nixpkgs.legacyPackages.aarch64-linux;
+  #  #  in
+  #  #  pkgs.buildGoModule {
+  #  #    pname = "vault";
+  #  #    version = "1.9.1";
+  #  #    src = pkgs.fetchFromGitHub {
+  #  #      owner = "hashicorp";
+  #  #      repo = "vault";
+  #  #      rev = "v1.17.0";
+  #  #      sha256 = null;
+  #  #    };
+  #  #    vendorHash = "sha256-VUiBfJ+AJqnuBt5SawjVcldeMnRonxh0/JBNVPbLk+U=";
+
+  #  #    subPackages = [ "." ];
+  #  #  };
+  #};
 }
